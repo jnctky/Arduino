eb20244dbeb9e947be16e34326ac0380de9f7aeb
CJ <jnctky@gmail.com>
2016-03-17 15:09:01 +0800

    更新RFID破解代码

--- rfid-master/examples/rfid_crack_keys/rfid_crack_keys.ino
+++ rfid-master/examples/rfid_crack_keys/rfid_crack_keys.ino
@@ -28,16 +28,17 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define RST_PIN         5 //9           // Configurable, see typical pin layout above
-#define SS_PIN          53 //10          // Configurable, see typical pin layout above
+#define RST_PIN         5//9           // Configurable, see typical pin layout above
+#define SS_PIN          53//10          // Configurable, see typical pin layout above
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
 
 // Number of known default keys (hard-coded)
 // NOTE: Synchronize the NR_KNOWN_KEYS define with the defaultKeys[] array
-#define NR_KNOWN_KEYS   8
+#define NR_KNOWN_KEYS   13
 // Known keys, see: https://code.google.com/p/mfcuk/wiki/MifareClassicDefaultKeys
 byte knownKeys[NR_KNOWN_KEYS][MFRC522::MF_KEY_SIZE] =  {
+    {0xff, 0xff, 0xff, 0xff, 0xff, 0xff}, // FF FF FF FF FF FF = factory default
     {0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5}, // A0 A1 A2 A3 A4 A5
     {0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xb5}, // B0 B1 B2 B3 B4 B5
     {0x4d, 0x3a, 0x99, 0xc3, 0x51, 0xdd}, // 4D 3A 99 C3 51 DD
@@ -45,15 +46,18 @@ byte knownKeys[NR_KNOWN_KEYS][MFRC522::MF_KEY_SIZE] =  {
     {0xd3, 0xf7, 0xd3, 0xf7, 0xd3, 0xf7}, // D3 F7 D3 F7 D3 F7
     {0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff}, // AA BB CC DD EE FF
     {0x00, 0x00, 0x00, 0x00, 0x00, 0x00},  // 00 00 00 00 00 00
-    {0xff, 0xff, 0xff, 0xff, 0xff, 0xff}, // FF FF FF FF FF FF = factory default
-
+    {0x71, 0x4c, 0x5c, 0x88, 0x6e, 0x97},
+    {0x58, 0x7e, 0xe5, 0xf9, 0x35, 0x0f},
+    {0xa0, 0x47, 0x8c, 0xc3, 0x90, 0x91},
+    {0x53, 0x3c, 0xb6, 0xc7, 0x23, 0xf6},
+    {0x8f, 0xd0, 0xa4, 0xf2, 0x56, 0xe9}
 };
 
 /*
  * Initialize.
  */
 void setup() {
-    Serial.begin(57600);         // Initialize serial communications with the PC
+    Serial.begin(9600);         // Initialize serial communications with the PC
     while (!Serial);            // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
     SPI.begin();                // Init SPI bus
     mfrc522.PCD_Init();         // Init MFRC522 card
@@ -76,20 +80,27 @@ void dump_byte_array(byte *buffer, byte bufferSize) {
  *
  * @return true when the given key worked, false otherwise.
  */
-boolean try_key(MFRC522::MIFARE_Key *key)
+boolean try_key(MFRC522::MIFARE_Key *key, byte block)
 {
     boolean result = false;
     byte buffer[18];
-    byte block = 0;
+            
+    char s[16];
+    //byte block = 0;
     MFRC522::StatusCode status;
-    
+
     // Serial.println(F("Authenticating using key A..."));
     status = mfrc522.PCD_Authenticate(MFRC522::PICC_CMD_MF_AUTH_KEY_A, block, key, &(mfrc522.uid));
     if (status != MFRC522::STATUS_OK) {
         Serial.print(F("PCD_Authenticate() failed: "));
-        Serial.print(F("key:"));
+        Serial.print(F("Block ")); Serial.print(block);
+        Serial.print(F("key: ")); 
         dump_byte_array((*key).keyByte, MFRC522::MF_KEY_SIZE);
         Serial.println(mfrc522.GetStatusCodeName(status));
+
+        //mfrc522.PCD_Reset();
+        //mfrc522.PICC_HaltA();       // Halt PICC
+        //mfrc522.PCD_StopCrypto1();  // Stop encryption on PCD
         return false;
     }
 
@@ -97,8 +108,8 @@ boolean try_key(MFRC522::MIFARE_Key *key)
     byte byteCount = sizeof(buffer);
     status = mfrc522.MIFARE_Read(block, buffer, &byteCount);
     if (status != MFRC522::STATUS_OK) {
-        // Serial.print(F("MIFARE_Read() failed: "));
-        // Serial.println(mfrc522.GetStatusCodeName(status));
+        Serial.print(F("MIFARE_Read() failed: "));
+        Serial.print(F("Block ")); Serial.print(block);Serial.println(mfrc522.GetStatusCodeName(status));
     }
     else {
         // Successful read
@@ -122,6 +133,8 @@ boolean try_key(MFRC522::MIFARE_Key *key)
  * Main loop.
  */
 void loop() {
+    static byte lastUID[6]={0,};
+    
     // Look for new cards
     if ( ! mfrc522.PICC_IsNewCardPresent())
         return;
@@ -130,6 +143,15 @@ void loop() {
     if ( ! mfrc522.PICC_ReadCardSerial())
         return;
 
+    bool bDiff = false;
+    for(int i = 0;i<6;i++){
+      if(lastUID[i] != mfrc522.uid.uidByte[i]){
+        lastUID[i] = mfrc522.uid.uidByte[i];
+        bDiff=true;
+      }
+    }
+    if(!bDiff) return;
+    
     // Show some details of the PICC (that is: the tag/card)
     Serial.print(F("Card UID:"));
     dump_byte_array(mfrc522.uid.uidByte, mfrc522.uid.size);
@@ -140,16 +162,30 @@ void loop() {
     
     // Try the known default keys
     MFRC522::MIFARE_Key key;
-    for (byte k = 0; k < NR_KNOWN_KEYS; k++) {
-        // Copy the known key into the MIFARE_Key structure
-        for (byte i = 0; i < MFRC522::MF_KEY_SIZE; i++) {
-            key.keyByte[i] = knownKeys[k][i];
-        }
-        // Try the key
-        if (try_key(&key)) {
-            // Found and reported on the key and block,
-            // no need to try other keys for this PICC
-            break;
-        }
+    for (byte block=0; block < 16; block++) {
+      for (byte k = 0; k < NR_KNOWN_KEYS; k++) {
+          // Copy the known key into the MIFARE_Key structure
+          for (byte i = 0; i < MFRC522::MF_KEY_SIZE; i++) {
+              key.keyByte[i] = knownKeys[k][i];
+          }
+  
+          // Try the key
+          if (try_key(&key, block)) {
+              // Found and reported on the key and block,
+              // no need to try other keys for this PICC
+              //break;
+          }
+      }
     }
+
+    // Force crack
+    /*
+    for (byte i = 0; i < MFRC522::MF_KEY_SIZE; i++) {
+        key.keyByte[i] = 0xFF;
+    }
+    for(int i = 255; i>=0; i--){
+      key.keyByte[0]=i;
+      try_key(&key, 1);
+    }*/
+
 }
